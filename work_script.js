d5b7e8179c43f225e089212bbcbc4ae64130c655
Lizzie Potocsky <89850381+epotocsky@users.noreply.github.com>
2021-12-07 00:53:41 -0500

    Delete work_script.js

--- work_script.js
+++
@@ -1,7 +0,0 @@
-var imgs = document.getElementsByTagName("img");
-for(var i = 0; i < imgs.length; i++) {
-    var img = imgs[i];
-    img.onclick = function(event) {
-         window.location.href = this.id + '.html';
-    };
-}
